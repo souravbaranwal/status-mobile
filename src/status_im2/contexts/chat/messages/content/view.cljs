469668eb9cad5f9a48fe68ad2cef82ee0d7cd84a
Ibrahem <vampirekid017@gmail.com>
2023-05-04 06:49:14 +0300

    Clean

--- src/status_im2/contexts/chat/messages/content/view.cljs
+++ src/status_im2/contexts/chat/messages/content/view.cljs
@@ -105,7 +105,7 @@
           :style               {:border-radius 16
                                 :opacity       (if (and outgoing (= outgoing-status :sending)) 0.5 1)}
           :on-press            (fn []
-                                 (if (and platform/ios? @keyboard-shown)
+                                 (if platform/ios?
                                    (rn/dismiss-keyboard!)
                                    (when (and outgoing
                                               (not= outgoing-status :sending)
