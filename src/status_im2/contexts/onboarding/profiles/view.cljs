bfd002cc887388fdbaa0b05f597ec75be418018e
yqrashawn <namy.19@gmail.com>
2023-05-18 16:37:30 +0800

    feat: add ident ring for login profile picture
    Signed-off-by: yqrashawn <namy.19@gmail.com>

--- src/status_im2/contexts/onboarding/profiles/view.cljs
+++ src/status_im2/contexts/onboarding/profiles/view.cljs
@@ -72,11 +72,10 @@
                 {:content (fn [] [profile-options key-uid context])}]))
 
 (defn profile-card
-  [{:keys [name key-uid customization-color keycard-pairing last-index set-hide-profiles]
-    :as   multiaccount}
+  [{:keys [name key-uid customization-color keycard-pairing last-index set-hide-profiles]}
    index]
   (let [last-item?      (= last-index index)
-        profile-picture (:uri (first (:images multiaccount)))]
+        profile-picture (rf/sub [:multiaccounts/login-profiles-picture key-uid])]
     [quo/profile-card
      {:name                 name
       :login-card?          true
@@ -175,10 +174,10 @@
 
 (defn login-section
   [{:keys [set-show-profiles]}]
-  (let [{:keys [name customization-color error processing password]
-         :as   multiaccount} (rf/sub [:multiaccounts/login])
-        sign-in-enabled?     (rf/sub [:sign-in-enabled?])
-        profile-picture      (:uri (first (:images multiaccount)))]
+  (let [{:keys [key-uid name customization-color error
+                processing password]} (rf/sub [:multiaccounts/login])
+        sign-in-enabled?              (rf/sub [:sign-in-enabled?])
+        profile-picture               (rf/sub [:multiaccounts/login-profiles-picture key-uid])]
     [rn/keyboard-avoiding-view
      {:style                  style/login-container
       :keyboardVerticalOffset (- (safe-area/get-bottom))}
